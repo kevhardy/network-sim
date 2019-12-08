d9ad002f839d052d6ae90514563360a9fb413526
Kevin Hardy <khroyaldeus@gmail.com>
2019-12-08 16:10:07 -0600

    Arranged file descriptors to remain open during process duration to keep track of read offset

--- node.cpp
+++ node.cpp
@@ -9,9 +9,10 @@
 #include <vector>
 
 using std::cout;
-using std::unordered_set;
 using std::string;
 using std::to_string;
+using std::unordered_map;
+using std::unordered_set;
 
 class Node {
  public:
@@ -20,32 +21,40 @@ class Node {
   int dur;    // duration of process life in seconds
   char* msg;  // message string to be sent
   int delay;  // time to wait before transmitting message in seconds
-  unordered_set<int> neighbors;  // list of neighboring nodes by id
+  unordered_set<int> neighbors;   // list of neighboring nodes by id
+  unordered_map<int, int> files;  // file descriptors for each neighbor
 
   Node(int id = 0, int dur = 0, int dest = 0, char* msg = 0, int delay = 100)
-      : id(id), dur(dur), dest(dest), msg(msg), delay(delay), neighbors() {}
+      : id(id),
+        dur(dur),
+        dest(dest),
+        msg(msg),
+        delay(delay),
+        neighbors(),
+        files() {}
 
   void datalink_receive_from_channel() {
-    int fd, bytes, j;
+    int bytes;
     char buf[101];
 
-    for (auto n : neighbors) {
-      string filename = "from" + to_string(id) + "to" + to_string(n);
-      fd = open(filename.c_str(), (O_CREAT | O_RDONLY), 0x1c0);
+    // Looping through all channels
+    for (auto fdpair : files) {
+      int n = fdpair.first; // current neighbor
+      int fd = fdpair.second; // fd for this neighbor's channel
 
-      if (fd < 0) {
-      printf("Error opening\n");
-      exit(1);
-      }
-
-      while((bytes = read(fd, buf, 100)) != 0) {
+      printf("neighbor id %d read: start\n", n);
+      while ((bytes = read(fd, buf, 100)) != 0) {
         printf("Cur Buffer: %s\n", buf);
         printf("child: # bytes read %d which were: %s\n", bytes, buf);
+
+        /* for (int i = 0; i < bytes; i++) {
+          char cur = buf[i];
+        } */
       }
 
-      printf("child: done\n");
-      close(fd);
+      printf("neighbor id %d read: done\n\n", n);
     }
+    printf("host id %d datalink read: done\n\n", id);
   }
 };
 
@@ -62,6 +71,18 @@ int main(int argc, char* argv[]) {
     for (int i = 6; i < argc; i++) host.neighbors.insert(atoi(argv[i]));
   }
 
+  // Open/creating file channels for reading and save file descriptors in map
+  for (auto n : host.neighbors) {
+    string filename = "from" + to_string(host.id) + "to" + to_string(n);
+    int fd = open(filename.c_str(), (O_CREAT | O_RDONLY), 0x1c0);
+
+    if (fd < 0) {
+      printf("Error opening\n");
+      exit(1);
+    }
+    host.files[n] = fd;
+  }
+
   printf(
       "Node members:\n"
       "id: %d - dur: %d - dest: %d - message: \"%s\" - delay: %d\n",
@@ -70,10 +91,13 @@ int main(int argc, char* argv[]) {
   for (auto i : host.neighbors) cout << i << " ";
   cout << "\n\n";
 
-  for (int i=0; i < host.dur; i++) {
+  // Main program loop
+  for (int i = 0; i < host.dur; i++) {
     host.datalink_receive_from_channel();
 
-    
     sleep(1);
   }
+
+  // Closing all files
+  for (auto fd : host.files) close(fd.second);
 }
